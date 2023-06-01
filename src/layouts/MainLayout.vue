af52f45396eb08240a6266b0705af514304b7b00
zi <zalicrim@indracompany.com>
2023-05-31 23:20:36 -0300

    add: menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,14 +2,30 @@
   <q-layout view="hHh lpR fFf">
     <q-header bordered class="bg-white text-black">
       <q-toolbar>
-        <q-toolbar-title> easyHome </q-toolbar-title>
-
+        <q-toolbar-title> <span class="title">easyHome</span> </q-toolbar-title>
         <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="rightDrawerOpen" side="right" overlay bordered>
-      <!-- drawer content -->
+    <q-drawer
+      v-model="rightDrawerOpen"
+      side="right"
+      overlay
+      bordered
+      offset-top="64"
+    >
+      <!-- Conteúdo do menu lateral -->
+      <q-list>
+        <q-item>
+          <q-item-section>Item 1</q-item-section>
+        </q-item>
+        <q-item>
+          <q-item-section>Item 2</q-item-section>
+        </q-item>
+        <q-item>
+          <q-item-section>Item 3</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container class="page-container">
@@ -20,6 +36,21 @@
     </q-page-container>
   </q-layout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      rightDrawerOpen: false,
+    };
+  },
+  methods: {
+    toggleRightDrawer() {
+      this.rightDrawerOpen = !this.rightDrawerOpen;
+    },
+  },
+};
+</script>
 
 <style scoped>
 .page-container {
@@ -34,5 +65,9 @@
   width: 100%;
   padding: 24px;
   box-sizing: border-box;
+}
+
+.title {
+  font-weight: 600;
 }
 </style>
